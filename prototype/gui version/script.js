9c14cf15e0d5ccb533acfd5c6d3a15f447acbbd6
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2021-11-29 13:42:38 +0100

    Edit code

--- prototype/gui version/script.js
+++ prototype/gui version/script.js
@@ -1,61 +1,33 @@
 var randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber)
-var attempts =10;
-var lostMessage = "C'est raté !!";
+var winMessage = "Félicitations, vous avez gagné!";
+var itIsSmall;
+var itIsBigger;
+var userInput;
 
-
-
-var tentatives = document.getElementById("tentatives");
 var message = document.getElementById("message")
 
 
-function writeattempts () {
-    tentatives.innerHTML = attempts;
-    console.log(attempts)
-}
-
 
 function onSubmit (){
-    --attempts;
-    if(attempts > 0){
-        var userInput = document.getElementById("deviner-input").value;
-        if(isNaN(userInput) ){
-            alert("Please Enter a Number!")
-        } else {
-            isCorrect(userInput)
+    userInput = document.getElementById("deviner-input").value;
+    itIsSmall = "C'est plus petit que " + userInput + ", essayez plus"
+    itIsBigger = "C'est plus grand que " + userInput + ", essayez plus"
+
+    if (userInput == randomNumber){
+        message.innerHTML = winMessage;
+
+    } else if (userInput > randomNumber){
+        message.innerHTML = itIsSmall
+
+    } else if (userInput < randomNumber){
+        message.innerHTML = itIsBigger
+
         }
-        userInput.value = ""
-    } else {
-        message.innerHTML = lostMessage
-        tentatives.innerHTML = 0;
-    }
+        
+    userInput.value = "" 
 }
 
 
-
-
-
-
-function isCorrect(userInput) {
-    
-var itIsSmall = "C'est plus petit que " + userInput + ", essayez plus"
-var itIsBigger = "C'est plus grand que " + userInput + ", essayez plus"
-
-        if (userInput == randomNumber){
-            var winMessage = "Félicitations, vous avez gagné après " + attempts + " tentatives";
-
-            alert(winMessage);
-        } else if (userInput > randomNumber){
-            message.innerHTML = itIsSmall
-            writeattempts() 
-
-        } else if (userInput < randomNumber){
-            message.innerHTML = itIsBigger
-            writeattempts() 
-
-        }
-     
- 
-}
 
 
