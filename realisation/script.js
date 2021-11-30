fad8696a1476104744d7c79d48e15090abed9a7d
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2021-11-30 16:10:41 +0100

    CLean code

--- realisation/script.js
+++ realisation/script.js
@@ -1,25 +1,22 @@
+// variables
 var randomNumber;
 var attempts;
 var attempsOutput;
 var winMessage;
 var itIsSmall;
 var itIsBigger;
-
 var lostMessage;
 var attemptsMessage;
 var message;
 
-
+// Saiser : Enter
 lostMessage = "C'est raté !!";
 message = document.getElementById("message");
 attemptsMessage = document.getElementById("attemptsMessage");
 randomNumber = Math.floor(Math.random() * 100);
 attempts =10;
 
-function writeattempts () {
-    attemptsMessage.innerHTML = attempts;
-    console.log(attempts)
-}
+
 
 
 function onSubmit (){
@@ -28,12 +25,32 @@ function onSubmit (){
 
     if(attempts > 0){
         var userInput = document.getElementById("userInput").value;
-        console.log(userInput)
+        // Controle de saiser
         if(isNaN(userInput) || userInput=== undefined){
             alert("Please Enter a Number!")
         } else {
-            isCorrect(userInput)
-        }
+            // Traitment
+            itIsSmall = "C'est plus petit que " + userInput + ", essayez plus"
+            itIsBigger = "C'est plus grand que " + userInput + ", essayez plus"
+               if (userInput == randomNumber){
+                   if(attempts >= 8){
+                       winMessage = 'Bravo, vous etes un Génie !!!'
+                   } else {
+                       winMessage = "Félicitations, vous avez gagné apres " + attempsOutput ;
+                   }
+                   message.innerHTML = winMessage
+       
+               } else 
+                    if (userInput > randomNumber){
+                        message.innerHTML = itIsSmall
+                        attemptsMessage.innerHTML = attempts;
+                    }
+                    else  {
+                    message.innerHTML = itIsBigger
+                    attemptsMessage.innerHTML = attempts;
+                    }
+            
+                }
         userInput.value = ""
     } else {
         message.innerHTML = lostMessage
@@ -48,26 +65,7 @@ function onSubmit (){
 
 function isCorrect(userInput) {
         
-     itIsSmall = "C'est plus petit que " + userInput + ", essayez plus"
-     itIsBigger = "C'est plus grand que " + userInput + ", essayez plus"
-        if (userInput == randomNumber){
-            if(attempts >= 8){
-                winMessage = 'Bravo, vous etes un Génie !!!'
-            } else {
-                winMessage = "Félicitations, vous avez gagné apres " + attempsOutput ;
-            }
-            message.innerHTML = winMessage
-
-        } else if (userInput > randomNumber){
-            message.innerHTML = itIsSmall
-            writeattempts() 
-
-        } else if (userInput < randomNumber){
-            message.innerHTML = itIsBigger
-            writeattempts() 
-
-        }
-     
+    
  
 }
 
